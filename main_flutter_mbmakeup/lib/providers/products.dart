5b92b34b32fd3c05d7d4cb46ccf1ff4a94ac622b
DIMM96 <monroy.ivan96@gmail.com>
2020-04-17 05:31:44 -0500

    Se crea la lista de productos

---
+++ main_flutter_mbmakeup/lib/providers/products.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Maquillaje 1',
+      description: 'Es un excelente color',
+      price: 300.55,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Maquillaje 2',
+      description: 'Descripcion del maquillaje 2',
+      price: 598.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Maquillaje 3',
+      description: 'Descripcion del maquillaje 3',
+      price: 250.69,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'Maquillaje 4',
+      description: 'Descripcion del maquillaje 4',
+      price: 88.20,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void addProduct() {
+    notifyListeners();
+  }
+}
