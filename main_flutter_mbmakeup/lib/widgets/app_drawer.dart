2f910c59945b042c426eed3b81e657c4969753a2
DIMM96 <monroy.ivan96@gmail.com>
2020-04-19 18:47:58 -0500

    Se crea pantalla al presionar boton menu

--- main_flutter_mbmakeup/lib/widgets/app_drawer.dart
+++ main_flutter_mbmakeup/lib/widgets/app_drawer.dart
@@ -1,1 +1,37 @@
-ECHO is on.
+//Pantalla desplegable al dar click en el menù
+import 'package:flutter/material.dart';
+
+import '../screens/orders_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('BIENVENIDO A MB Makeup'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Tienda'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Órdenes de compra'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
