676faaa29092db0f95f913098876f2a454725037
DIMM96 <monroy.ivan96@gmail.com>
2020-04-19 14:59:52 -0500

    update a la clase products.dart

--- main_flutter_mbmakeup/lib/providers/products.dart
+++ main_flutter_mbmakeup/lib/providers/products.dart
@@ -6,17 +6,17 @@ class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
-      title: 'Maquillaje 1',
+      title: 'Labial',
       description: 'Es un excelente color',
-      price: 300.55,
+      price: 80.55,
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
-      title: 'Maquillaje 2',
+      title: 'Sombras',
       description: 'Descripcion del maquillaje 2',
-      price: 598.99,
+      price: 90.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
@@ -24,7 +24,7 @@ class Products with ChangeNotifier {
       id: 'p3',
       title: 'Maquillaje 3',
       description: 'Descripcion del maquillaje 3',
-      price: 250.69,
+      price: 79.99,
       imageUrl:
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
