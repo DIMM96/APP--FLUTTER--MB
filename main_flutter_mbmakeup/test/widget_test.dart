d6b2c643c34d5e57f28965eee2f07eb5c56fdfc0
DIMM96 <monroy.ivan96@gmail.com>
2020-04-19 18:54:53 -0500

    se importan paquete necesarios de la app

--- main_flutter_mbmakeup/test/widget_test.dart
+++ main_flutter_mbmakeup/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:main_flutter_mbmakeup/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
