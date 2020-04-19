ecf519cf9db7a27dba26370537c0aee5a2e28f6b
DIMM96 <monroy.ivan96@gmail.com>
2020-04-19 18:38:55 -0500

    update a productos.dart

--- main_flutter_mbmakeup/lib/providers/products.dart
+++ main_flutter_mbmakeup/lib/providers/products.dart
@@ -16,23 +16,23 @@ class Products with ChangeNotifier {
       id: 'p2',
       title: 'Sombras',
       description: 'Descripcion del maquillaje 2',
-      price: 90.99,
+      price: 55.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
-      title: 'Maquillaje 3',
+      title: 'Delineador',
       description: 'Descripcion del maquillaje 3',
-      price: 79.99,
+      price: 22.99,
       imageUrl:
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
-      title: 'Maquillaje 4',
+      title: 'Rimel',
       description: 'Descripcion del maquillaje 4',
-      price: 88.20,
+      price: 10.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
