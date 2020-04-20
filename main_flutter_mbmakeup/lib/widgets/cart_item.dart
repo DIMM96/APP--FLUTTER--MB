016843bb70b8649257fe5c8febf0ee1d0289f503
DIMM96 <monroy.ivan96@gmail.com>
2020-04-19 23:31:24 -0500

    Se modifican decimales de descripciones

--- main_flutter_mbmakeup/lib/widgets/cart_item.dart
+++ main_flutter_mbmakeup/lib/widgets/cart_item.dart
@@ -58,7 +58,7 @@ class CartItem extends StatelessWidget {
               ), //Tamaño del texto dentro de los circulos morados
             ),
             title: Text(title),
-            subtitle: Text('Total: \$${(price * quantity)}'),
+            subtitle: Text('Total: \$${(price * quantity).toStringAsFixed(2)}'),
             trailing: Text('$quantity x'),
           ),
         ),
