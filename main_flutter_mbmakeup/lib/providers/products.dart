27b408aa83bf18b73a21a4216a74fd87773d9633
DIMM96 <monroy.ivan96@gmail.com>
2020-04-21 09:27:13 -0500

    se modifica var imageUrl

--- main_flutter_mbmakeup/lib/providers/products.dart
+++ main_flutter_mbmakeup/lib/providers/products.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/foundation.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
@@ -9,32 +9,28 @@ class Products with ChangeNotifier {
       title: 'Labial',
       description: 'Es un excelente color',
       price: 10.00,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+      imageUrl: 'images/labial.jpg',
     ),
     Product(
       id: 'p2',
       title: 'Sombras',
       description: 'Descripcion del maquillaje 2',
       price: 20.00,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+      imageUrl: 'images/sombras.jpg',
     ),
     Product(
       id: 'p3',
       title: 'Delineador',
       description: 'Descripcion del maquillaje 3',
       price: 30.00,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+      imageUrl: 'images/delineador.jpg',
     ),
     Product(
       id: 'p4',
       title: 'Rimel',
       description: 'Descripcion del maquillaje 4',
       price: 40.00,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+      imageUrl: 'images/rimel.jpg',
     ),
   ];
 
